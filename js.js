d9d70ee61867fb4876acab6f4ebd2cc77ed112da
Timatima1703 <Tolik170395@mail.ru>
2025-02-16 17:11:34 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+<button="externalLinkButton">Перейти на сайт</button>
+
+<script>
+  document.getElementById('externalLinkButton').addEventListener('click', function() {
+    window.location.href = 'https://www.example.com';
+  });
+</script>
